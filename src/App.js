3511bec65f670cffc0647847b44abe01effe767f
user name <user email>
2022-09-07 13:10:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="main">
+        <section class="section1">
+            <h1> Learn to paint by  watching others</h1>
+            <p> <span id="p-span"> See how experienced painters create art in real-time.</span> <span id="p-span"> Watching scripted tutorials is great, </span> <span id="p-span"> but understand artists </span> <span id="p-span"> think is invaluable. </span> </p>
+        </section>
+        
+        
+        <section>
+            <div class="try-it">
+                <p>Try it free for 7 days then $20/mo there after</p>
+            </div>
+            <form class="form">
+                <input type="text" placeholder="Full Name"  required autocomplete="on"/>
+                <input type="text" placeholder="Last Name" required />
+                <input type="email" placeholder="Email Address" required />
+                <input type="password" placeholder="Password" required />
+                <button type="submit" id="btn">CLAIM YOUR FREE TRAIL</button>
+                
+                <p>By clicking the button you are accepting <a href="https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwjHpOXWy__5AhXFoVwKHaxcA2UQPAgI">Terms & Conditions</a> </p>
+        
+            </form>
+        </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
